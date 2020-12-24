7d5028db31f565056c8c65e5861d83ee34e5ee81
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2020-12-24 07:03:06 -0600

    Create AutomaticImports.scala

---
+++ week_03/AutomaticImports.scala
@@ -0,0 +1,13 @@
+/*
+All members of package scala, java.lang      
+and members of singleton object scala.Predef.
+
+Some types and functions seen in the course so far: 
+Int                 scala.Int
+Boolean             scala.Boolean
+Object              java.lang.Object
+require             scala.Predef.require
+assert              scala.Predef.assert
+
+More info: www.scala-lang.org/api/current
+*/
