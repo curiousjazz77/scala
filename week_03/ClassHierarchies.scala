f959fdaab1a2713866cf80838b61d393a24af05a
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2020-12-24 06:32:47 -0600

    Update ClassHierarchies.scala

--- week_03/ClassHierarchies.scala
+++ week_03/ClassHierarchies.scala
@@ -1,5 +1,8 @@
 objet intsets {
 println("Welcome to the scala worksheet")
+  val t1 = new NonEmpty(3, new Empty, new Empty)    // t1: NonEmpty = {.3.}
+  val t2 = t1 incl 4 // 4 gets added                // t2: IntSet = {.3{.4.}} invariant held
+  
 }
 
 abstract class IntSet{
@@ -10,4 +13,22 @@ abstract class IntSet{
 class Empty extends Intset {
   def contains(x:Int): Boolean = false
   def incl(x:Int): IntSet = new NonEmpty(x, new Empty, new Empty)
+  override def toString = "." // simple example
+ }
+
+
+class NonEmpty(elem: Int, left: IntSet, right:IntSet) extends Intset {
+  
+  def contains(x:Int): Boolean = 
+    if (x < elem) left contains x
+    else if (x > elem) right contains x //recursive call
+    else true
+  
+  //persistent data structure. this doesnt overwrite previous nodes, instead places on top of
+  def incl(x:Int): IntSet = 
+    if (x < elem) new NonEmpty(elem, left incl x, right) //keep going until find empty place to insert
+    else if (x > elem) new NonEmpty(elem, left, right incl x)
+    else this
+  
+  override def toString = "{" + left + elem + right + "}"
  }
