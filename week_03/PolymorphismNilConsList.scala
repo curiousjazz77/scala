6c1f35978e52a732335fced1c08a50eb779c9a56
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2020-12-24 07:42:37 -0600

    Update PolymorphismNilConsList.scala

--- week_03/PolymorphismNilConsList.scala
+++ week_03/PolymorphismNilConsList.scala
@@ -29,12 +29,38 @@ class Nil extends IntList
 
 
 //Since it's too narrow to define lists with Int element, we will generalize using a type parameter
+package week3
 
-trait List[T]
+trait List[T] {
+  def isEmpty: Boolean
+  def head: T
+  def tail: List[T]
+}
 
 //two subclasses Cons and Nil
 
 // val does two things here: define the parameter of the class and field (value definition of the class)
 // two fields head and tail become initialized (preferred as a syntax form since is precise)
-class Cons[T](val head: Int, val tail: IntList) extends List[T]
-class Nil[T] extends List[T]
+class Cons[T](val head: Int, val tail: IntList) extends List[T] {
+  def isEmpty: Boolean = false //cons cells are never empty
+  
+}
+
+class Nil[T] extends List[T] {
+  def isEmpty: Boolean = true
+  def head: Nothing = throw NoSuchElementException("Nil.head")
+  def tail: Nothing = throw NoSuchElementException("Nil.tail")
+}
+
+/*
+Like classes, functions can have type parameters
+
+For instance, here is a function that creates a list consisting of a single element
+
+def singleton[T](elem: T) = new Cons[T](elem, new Nil[T])
+
+We can then write:
+
+singleton[Int](1)
+singleton[Boolean](true)
+
