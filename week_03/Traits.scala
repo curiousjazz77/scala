980a85d88e2b775870acb127403d5a275df5c725
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2020-12-24 07:07:35 -0600

    Update Traits.scala

--- week_03/Traits.scala
+++ week_03/Traits.scala
@@ -1,5 +1,5 @@
 /*
-In Java, as well as in Scala, a class can only have ONE superclass
+In Java, as well as in Scala, a class can only have ONE superclass (single inheritance)
 
 But what if a class has several natural supertypes to which it confirm or from
 which it wants to inherit code?
@@ -7,6 +7,14 @@ which it wants to inherit code?
 Here, you can use `traits`
 
 A trait is declared like an abstract class, just with `trait` instead of `abstract class`
+
+Example:
+  class Square extends Shape with Planar with Movable
+  
+Traits resemble interfaces in Java, but are more powerful because
+they can contain fields and concrete methods.
+
+On the other hand, traits can't have (value) parameters, only classes can
 */
 
 trait Planar {
